de09b9a51f1a834c54b09be07b5fc0d6c317c42f
x4L0 <artfil2004@gmail.com>
2025-05-05 10:09:02 -0400

    Fixing stuff

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 # -----------------------------
 # main.py
 # -----------------------------
-# main.py
-
 import numpy as np
-import matplotlib.pyplot as plt
 from src.truth_function import f_truth
 from src.utils import generate_data, rescale
 from src.plot import plot_chi2_vs_model_complexity
+from numpy.polynomial.chebyshev import chebfit, chebval
+import warnings
+
+warnings.simplefilter('ignore', np.RankWarning)
 
 # Parameters
-num_points = 500
-x_range = (0, 10)
-noise_level = 0.1
-max_params = 15
-random_state = 42
+num_points = 100
+x_range = (-0.2, 1.2)
+noise_level = 0.2
+max_degree = 15
+random_seed = 42
 
-# Generate x and data
-x_data = np.linspace(*x_range, num_points)
-y_data, y_truth, y_errors = generate_data(x_data, f_truth, noise_level=noise_level, random_state=random_state)
+# Generate datasets A and B
+rng = np.random.default_rng(random_seed)
+x_data = np.linspace(x_range[0], x_range[1], num_points)
+rng.shuffle(x_data)
+x_A, x_B = np.array_split(x_data, 2)
 
-# Split into two datasets
-split_index = int(0.7 * num_points)
-x_A, y_A, err_A = x_data[:split_index], y_data[:split_index], y_errors[:split_index]
-x_B, y_B, err_B = x_data[split_index:], y_data[split_index:], y_errors[split_index:]
+y_A, y_A_truth = generate_data(f_truth, x_A, noise_level, random_state=0)
+y_B, y_B_truth = generate_data(f_truth, x_B, noise_level, random_state=1)
 
-# Run cross-validation and plot
-plot_chi2_vs_model_complexity(x_A, y_A, err_A, x_B, y_B, err_B, max_params)
+# Chebyshev basis fitting with rescaled x values
+x_A_scaled = rescale(x_A)
+x_B_scaled = rescale(x_B)
+
+degrees = np.arange(1, max_degree + 1)
+chi2_A = []
+chi2_B = []
+
+for deg in degrees:
+    coefs = chebfit(x_A_scaled, y_A, deg)
+    y_fit_A = chebval(x_A_scaled, coefs)
+    y_fit_B = chebval(rescale(x_B), coefs)
+
+    chi2_A_val = np.sum(((y_A - y_fit_A) / (noise_level * y_A_truth))**2) / len(y_A)
+    chi2_B_val = np.sum(((y_B - y_fit_B) / (noise_level * y_B_truth))**2) / len(y_B)
+
+    chi2_A.append(chi2_A_val)
+    chi2_B.append(chi2_B_val)
+
+plot_chi2_vs_model_complexity(degrees, chi2_A, chi2_B)
--- src/utils.py
+++ src/utils.py
@@ -14,10 +14,10 @@ def generate_data(x: np.ndarray, truth_fn, noise_level: float, random_state=None
     y_truth = truth_fn(x)
     noise = rng.normal(loc=0.0, scale=noise_level * y_truth)
     y_noisy = y_truth + noise
-    y_noisy = np.clip(y_noisy, 0, None)  # ensure non-negative
-    return y_noisy, y_truth, noise
+    y_noisy = np.clip(y_noisy, 0, None)
+    return y_noisy, y_truth
 
-def rescale(x: np.ndarray, new_min: float, new_max: float) -> np.ndarray:
+def rescale(x: np.ndarray, new_min: float = -1, new_max: float = 1) -> np.ndarray:
     """
     Rescale x to the interval [new_min, new_max].
     """
