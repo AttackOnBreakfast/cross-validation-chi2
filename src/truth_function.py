86cbd75e454eb5bc6d8a1014efc225bb7476b1b8
x4L0 <artfil2004@gmail.com>
2025-05-05 01:35:05 -0400

    Truth update

--- src/truth_function.py
+++ src/truth_function.py
@@ -1,5 +1,11 @@
 # -----------------------------
 # src/truth_fuction.py 
 # -----------------------------
-def f_truth(x):
-    return 3 * (x + 0.2)**1.2 * (1.2 - x)**1.2 * (1 + 2.3 * x)
+import numpy as np
+
+def truth_function(x: np.ndarray) -> np.ndarray:
+    """
+    Defines the ground truth function f_truth(x).
+    For example, a 7th-degree polynomial.
+    """
+    return 2 + x - 0.5 * x**2 + 0.1 * x**3 - 0.05 * x**4 + 0.02 * x**5 - 0.005 * x**6 + 0.001 * x**7
